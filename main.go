7ce4eb9bb2eb6c08980728f1f4869f7f75c2b91d
olup <loup.topalian@gmail.com>
2022-07-21 22:17:16 +0200

    feat: update icon

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:            "lunii-tools",
-		Width:            1024,
-		Height:           768,
-		Assets:           assets,
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		Title:  "lunii-admin",
+		Width:  1024,
+		Height: 768,
+		Assets: assets,
+		//BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
